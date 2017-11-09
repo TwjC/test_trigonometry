fd1669849d6ce5e05ed06404e27896e6a47ad776
Tom Callewaert <t.w.j.callewaert@tudelft.nl>
2017-11-09 13:28:26 +0100

    iteration2

--- test.py
+++ test.py
@@ -4,3 +4,14 @@ This file is now a test to commit changes
 
 #%% Import required modules 
 import numpy as np 
+
+#%% Def the required functions 
+def circumference(radius):
+	circumference = (radius *2*np.pi)
+	return circumference
+
+
+#%% Execute the code 
+radius = 0.5 
+print('the circumference of a circle with  radius {} = {}'.format(radius, circumference(radius)))
+
